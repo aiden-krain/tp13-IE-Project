079cdeff471bc2fa245b888ad6a4e1ad9734e61f
Tricia <tnjo0001@student.monash.edu>
2022-09-01 16:30:22 +1000

    update to accommodation page

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,jsonify
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('Home.html')
-@app.route('/recommendation')
+
+@app.route('/recommendation', methods=["POST", "GET"])
 def get_recommendation():
     return render_template('recommendation.html')
 
-@app.route('/accommodation')
+
+suburbs = ['Albert Park-Middle Park-West St Kilda','Armadale','Carlton North','Carlton-Parkville','CBD-St Kilda Rd'
+    ,'Collingwood-Abbotsford','Docklands','East Melbourne','East St Kilda','Elwood','Fitzroy',
+     'Fitzroy North-Clifton Hill','Flemington-Kensington','North Melbourne-West Melbourne',
+'Port Melbourne','Prahran-Windsor','Richmond-Burnley','South Melbourne','South Yarra','Southbank','St Kilda','Toorak',]
+@app.route('/accommodation', methods=["POST", "GET"])
 def get_accommodation():
-    return render_template('accommodation.html')
+    if request.method == "GET":
+        #search = request.args.get('autocomplete')
+        #app.logger.debug(search)
+        return render_template('accommodation.html', suburbs=suburbs)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
